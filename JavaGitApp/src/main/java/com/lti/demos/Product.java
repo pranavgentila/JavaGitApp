f29e9eb1b9529e86027df78ed3277d44ca129f4f
pranavgentila <pranavgentila03@gmail.com>
2021-12-01 14:25:12 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demos/Product.java
@@ -0,0 +1,18 @@
+package com.lti.demos;
+
+public class Product {
+	
+	private int prdId;
+	private String prdName;
+	public Product(int prdId, String prdName) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+	}
+	@Override
+	public String toString() {
+		return "Product [prdId=" + prdId + ", prdName=" + prdName + "]";
+	}
+	
+
+}
